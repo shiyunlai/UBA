727c1692516648e8b209a0bb2b854f8f33f4413e
shiyunlai <shiyunlai@gmail.com>
2015-05-27 15:28:45 +0800

    add new File AddTools

---
+++ org.yunlai.uba.core/src/main/java/org/yunlai/uba/core/AddTools.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.yunlai.uba.core;
+
+/**
+ * @author mega-t420
+ *
+ */
+public class AddTools {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Add Tools");
+	}
+
+}
